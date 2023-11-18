43ac7108d8edd1e84d311ea750919177003c799c
garimas007 <singhgarima54@gmail.com>
2023-11-18 23:29:38 +0530

    commiting the changes in 1st assignment

--- python_assignment_1.py
+++ python_assignment_1.py
@@ -1,1 +1,29 @@
 print("This is Assignment1")
+
+import re
+
+def check_password_strength(password):
+    special_char = r"[!@#$%^&*(),.?\":{}|<>]"
+
+    if len(password) < 8:
+        return False
+    if not any(i.isupper() for i in password) or not any(i.islower() for i in password):
+        return False
+    if not any(i.isdigit() for i in password):
+        return False
+    if not re.search(special_char, password):
+        return False
+    return True
+
+def main():
+    password = input("Enter your password: ")
+
+    output = check_password_strength(password)
+
+    if output:
+        print("Password is strong")
+    else:
+        print("password is weak")
+
+if __name__=="__main__":
+    main()
